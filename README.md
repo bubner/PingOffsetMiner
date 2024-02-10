5eed2d92c8d1ab650df8947727adde3e62a1a0fe
Lucas Bubner <lkbubner@proton.me>
2024-02-10 19:26:03 +1030

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# PingOffsetMiner
+### Forge 1.8.9 Hypixel Skyblock
+A mod that takes into account ping offset when mining ores in Hypixel Skyblock, allowing for faster mining.
+
+###### Copyright (c) Lucas Bubner 2024, under MIT.
---
+++ src/main/java/holo/pingoffsetminer/PingOffsetMiner.java
@@ -0,0 +1,22 @@
+package holo.pingoffsetminer;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+/**
+ * PingOffsetMiner
+ * @author Lucas Bubner, 2024
+ */
+@Mod(modid = PingOffsetMiner.MODID, version = PingOffsetMiner.VERSION)
+public class PingOffsetMiner {
+    public static final String MODID = "PingOffsetMiner";
+    public static final String VERSION = "1.0";
+
+    @Mod.EventHandler
+    public void preInit(FMLInitializationEvent event) {
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+    }
+}
